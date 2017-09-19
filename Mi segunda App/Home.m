debf0db545cd66c10fb17497993f7f6f17a9ea0f
Miguel Angel Larios Torres <mlariospc@corporativosade.com.mx>
2017-09-19 16:45:39 -0500

    MOD: FINAL

--- Mi segunda App/Home.m
+++ Mi segunda App/Home.m
@@ -19,7 +19,7 @@ static NSArray *_myColorArray = nil;
 static NSArray *_value = nil;
 
 int value =0;
-//desoues de cargar la vista se va a esta funcion
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -32,7 +32,6 @@ int value =0;
                      [UIColor greenColor],[UIColor blackColor], nil];
     
     if (value == [_value count]) {
-
         value =0;
     }
     
@@ -40,35 +39,8 @@ int value =0;
     self.lblWelcome.textColor = [_myColorArray objectAtIndex:value];
     value ++;
     
-    /*myCounter++;
-    if (myCounter == 6) {
-        myCounter = 0;
-    }
-    switch (myCounter) {
-        case 1:
-            self.lblWelcome.text = @"Primer click!";
-            self.lblWelcome.textColor = [UIColor whiteColor];
-            break;
-        case 2:
-            self.lblWelcome.text = @"Segundo click!";
-            self.lblWelcome.textColor = [UIColor blackColor];
-            break;
-        case 3:
-            self.lblWelcome.text = @"Tercero click!";
-            self.lblWelcome.textColor = [UIColor redColor];
-            break;
-        case 4:
-            self.lblWelcome.text = @"Cuarto click!";
-            self.lblWelcome.textColor = [UIColor greenColor];
-            break;
-        case 5:
-            self.lblWelcome.text = @"Quinto click!";
-            self.lblWelcome.textColor = [UIColor redColor];
-            myCounter = 0;
-            break;
-    }*/
-    
 }
+
 
 
 - (void)didReceiveMemoryWarning {
